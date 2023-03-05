1a1a0616c3843d9046820d014aa7b9d35758df9c
Rafael Justo <rafael.justoscherer@gmail.com>
2023-03-05 14:48:04 -0300

    :construction: adicionando tipografia e progresso na home

--- lib/views/home/home.view.dart
+++ lib/views/home/home.view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_games/utils/app_colors.dart';
+import 'package:flutter_games/utils/app_images.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -10,13 +12,34 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
+    double logoSize = 35.0;
+
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blue[700],
-        title: const Text('Flutter Games'),
+        backgroundColor: AppColors.mainColor,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox.square(
+              dimension: logoSize,
+              child: Image.asset(AppImages.logo),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 10.0, right: 10.0 + logoSize),
+              child: const Text(
+                'Flutter Games',
+                style: TextStyle(
+                  fontFamily: 'Gilam',
+                  color: AppColors.fontColor,
+                  fontSize: 24.0,
+                ),
+              ),
+            ),
+          ],
+        ),
         centerTitle: true,
       ),
-      body: Container(color: Colors.lightBlue),
+      body: Container(color: AppColors.bgColor),
     );
   }
 }
