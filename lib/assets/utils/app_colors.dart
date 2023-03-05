300c1dd5f41799c8fea3bbf34a43d24c417eb651
Rafael Justo <rafael.justoscherer@gmail.com>
2023-03-05 14:17:58 -0300

    :sparkles: criando classe auxiliar para cores do app

---
+++ lib/assets/utils/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+mixin AppColors {
+  /// Tons de azul
+  static const Color mainColor = Color(0xFF00B5F8);
+  static const Color bgColor = Color(0xFF91DEFF);
+  static const Color secondaryColor = Color(0xFF00569E);
+
+  /// Tons de preto
+  static const Color fontColor = Color(0xff1B1D1E);
+}
