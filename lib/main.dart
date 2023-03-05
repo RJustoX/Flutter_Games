84924abc54868d50739955a6aef56aee3c21686f
Rafael Justo <rafael.justoscherer@gmail.com>
2023-03-05 19:06:23 -0300

    :construction: preparando o hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_games/views/home/home.view.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+
   runApp(const MyApp());
 }
 
